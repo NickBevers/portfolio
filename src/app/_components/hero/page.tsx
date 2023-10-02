15ef7ad17051550308c6dba5ee1647db6ede5cc9
Nick Bevers <nick.bevers9@gmail.com>
2023-10-02 21:28:36 +0200

    feat: add video in heroSection and aboutSection to the homepage

--- src/app/_components/hero/page.tsx
+++ src/app/_components/hero/page.tsx
@@ -1,10 +1,36 @@
-export default function Hero() {
+'use client';
+// import { useEffect } from 'react';
+import styles from './styles.module.css'
+
+function scrollDown() {
+    window.scrollTo(0, 2000);
+}
+
+export default function HeroSection() {
+
+    // useEffect(() => {
+    //     const video = document.querySelector('video');
+    //     if (!video) return;
+    //     // video.playbackRate = 0.85;
+    // }, [])
+
     return (
-        <div className="hero">
-        <h1 className="title">
-            Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-        {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
+        <div className={styles.hero}>
+            {/* <h1 className={styles.title}>HeroSection</h1> */}
+
+            <video autoPlay muted loop preload='auto' className={styles.video}>
+                <source src="/videos/heroVideo.webm" type="video/webm"/>
+                <source src="/videos/heroVideo.mp4" type="video/mp4"/>
+            </video>
+            <div className={styles.overlay}></div>
+
+            
+            <h1 className={styles.heroText}>HERO</h1>
+
+
+            <div className={styles.scrollDown} onClick={scrollDown}>
+                <p>Scroll to discover</p>
+            </div>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-// import Image from 'next/image'
-import Hero from './_components/hero/page'
-import styles from './page.module.css'
+import HeroSection from './_components/hero/page';
+import AboutSection from './_components/about/page'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-        <Hero />
-    </main>
+    <div>
+        <HeroSection />
+        <AboutSection />
+    </div>
   )
 }
