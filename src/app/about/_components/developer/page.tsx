b8418217fde4287fc460b5dbf4bda178bc8de9eb
Nick Bevers <nick.bevers9@gmail.com>
2023-10-05 14:44:01 +0200

    add developer section and about content to the about page

--- src/app/about/_components/developer/page.tsx
+++ src/app/about/_components/developer/page.tsx
@@ -1,5 +1,15 @@
+'use client'
 import styles from './styles.module.css'
 import Image from 'next/image';
+import { useRef } from "react";
+import { useDraggable } from "react-use-draggable-scroll";
+
+interface WheelEvent<T = Element> extends React.MouseEvent<T, globalThis.WheelEvent> {
+    deltaMode: number;
+    deltaX: number;
+    deltaY: number;
+    deltaZ: number;
+}
 
 const strengths: Array<{name: string, image: string, alt: string}> = [
     {
@@ -34,27 +44,42 @@ const strengths: Array<{name: string, image: string, alt: string}> = [
     },
 ];
 
+const handleScroll = (e: WheelEvent) => {
+    const imageContainer = document.querySelector('#strengths') as HTMLDivElement;
+    imageContainer.scrollTo({
+        left: imageContainer.scrollLeft + e.deltaY *1.5,
+        behavior: 'smooth'
+    })
+}
+
+const toggleScroll = (scrollStyle: string) => {
+    document.querySelector('html')!.style.overflow = scrollStyle;
+}
+
 export default function Developer() {
+    const ref = useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
+    const { events } = useDraggable(ref, {safeDisplacement: 11});
+
     return (
         <div className={styles.developer__container}>
-            <h1>Developer <span className={styles.superScript}>(1)</span></h1>
-            <p className={styles.developer__description}>This is the developer section of my about page</p>
+            <h2 className={styles.developer__title}>Developer <span className={styles.superScript}>(1)</span></h2>
+            <p className={styles.developer__description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lectus eget pellentesque in nisi. Consequat tristique id ut nulla pretium cras. Dolor mi nullam facilisis nunc sagittis bibendum volutpat.</p>
 
             <div className={styles.strength__container}>
-                <h5>Strengths</h5>
-                <div className={styles.strength__roulette}>
+                <h5 className={styles.strength__title}>Strengths</h5>
+                <div className={styles.strength__roulette} id='strengths' onWheel={(e) => {handleScroll(e)}} onMouseEnter={()=>{toggleScroll('hidden')}} onMouseLeave={()=>{toggleScroll('auto')}} ref={ref}>
                     {strengths.map((strength, index) => {
                         return (
-                            <div key={index} className={styles.strength__item}>
+                            <div key={index} {...events} className={styles.strength__item}>
                                 <div className={styles.strength__image_container}>
-                                    <Image src={strength.image} alt={strength.alt} width={0} height={0} sizes="100vw" style={{ width: '100%', height: 'auto' }}/>
+                                    <Image src={strength.image} alt={strength.alt} width={0} height={0} sizes="100vw" className={styles.strength__image}/>
+                                    <div className={styles.strength__image_overlay}></div>
                                 </div>
-                                <p className={styles.strength__name}>{strength.name}</p>
+                                <p className={styles.strength__name}>{`${index + 1}. ${strength.name}`}</p>
                             </div>
                         );
                     })}
                 </div>
-
             </div>
 
             <hr className={styles.divider} />
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -24,6 +24,8 @@ export default function About() {
             </div>
 
             <Developer />
+
+            
         </div>
     );
 }
