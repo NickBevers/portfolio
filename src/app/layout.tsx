da4838a069ad96a6e197c1f7f00d0be874b26442
Nick Bevers <nick.bevers9@gmail.com>
2024-04-10 21:00:37 +0200

    style add improved styling for the font and projectCard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 const poppins = Poppins({
   subsets: ["latin"],
   display: "swap",
-  weight: ["200", "400", "500", "600", "700", "800"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
