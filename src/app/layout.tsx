154a26e3d0471f08a097c93ee8ce0572e368a3d5
NickBevers <nick.bevers9@gmail.com>
2024-04-10 14:20:03 +0200

    feat: change fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,23 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import TTHoves from "./styles/fonts";
 import { ProjectProvider } from "@/context/globalState";
+import { Poppins } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Nick Bevers",
   description: "Your friendly neighborhood web developer",
 };
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["200", "400", "500", "600", "700", "800"],
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={TTHoves.className}>
+      <body className={poppins.className}>
         <ProjectProvider>
           {children}
         </ProjectProvider>
